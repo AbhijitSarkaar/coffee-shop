538ba1ff1a88a5acec369ecada1c253593696c98
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-12-29 08:58:52 +0530

    setup express server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const HtmlWebpackplugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./build"),
+    path: path.resolve(__dirname, "./public/build"),
     filename: "bundle.js",
   },
+  plugins: [
+    new HtmlWebpackplugin({
+      template: "./src/template.html",
+      filename: path.resolve(__dirname, "./public/index.html"),
+    }),
+  ],
 };
